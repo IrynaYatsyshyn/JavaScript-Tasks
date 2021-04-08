95abb2da0e8f54235c7ed772d8098cea93f13db1
200221 <jacushin2002@gmail.com>
2021-04-08 23:46:07 +0300

    doing some task

--- Task4/main.js
+++ Task4/main.js
@@ -44,34 +44,53 @@
 //     console.log(exception.stack);
 // }
 
-
-
-
-//-------------------------------------------------------
 //TASK3
 // class MonthException {
 //     constructor(message) {
-//         name = "MonthExceptions";
+//         this.name = "MonthExceptions";
+//         this.message = message;
 //     }
 // }
 //
 // function showMonthName(month) {
-//     const monthNames = ["", "January", "February", "March", "April", "May", "June",
+//     month = month - 1;
+//     const monthNames = ["January", "February", "March", "April", "May", "June",
 //         "July", "August", "September", "October", "November", "December"];
-//     console.log(monthNames[16]);
-//     if (monthNames >= 13) {
+//     if (monthNames[month] !== undefined) {
+//         return monthNames[month];
+//     } else {
 //         throw new Error("Incorrect month number!");
 //     }
-//     return monthNames;
 // }
 //
-// showMonthName();
-//
 // try {
-//     console.log("correct!");
-//
+//     console.log(showMonthName(16));
 // } catch (exception) {
 //     console.log(exception.name);
 //     console.log(exception.message);
 //     console.log(exception.stack);
 // }
+
+//TASK4
+// function showUser(id) {
+//     if (id < 0) {
+//         throw new Error(id + " - Choose another ID!ID must be positive!")
+//     }
+//     return {id: id};
+// }
+// function showUsers(ids) {
+//     let array = [];
+//     ids.filter(function (id) {
+//         try {
+//             let result = showUser(id);
+//             console.log(result);
+//             array.push(result);
+//         } catch (exceptions) {
+//             console.log(exceptions.name);
+//             console.log(exceptions.message);
+//         }
+//     });
+// }
+//
+// showUsers([7, -12, 22, 44]);
+
