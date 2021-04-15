b4c5506b4efb0f8e5606ad68dbe2dcc295882829
200221 <jacushin2002@gmail.com>
2021-04-15 17:48:03 +0300

    doing task

---
+++ Task6/main.js
@@ -0,0 +1,44 @@
+// TASK1
+// let getElemByTag = document.querySelector("div");
+// getElemByTag.innerHTML = "Second";
+// let getElem = document.getElementById("test");
+// getElem.innerHTML = "Second";
+//
+//TASK2
+// document.getElementById("image").src = "cat.jpg";
+//
+//TASK3 -------------------------------------- change
+// let getElem = document.createElement("Selector text 0:");
+// console.log(getElem);
+//TASK4 --------------------------------------- change
+//
+//TASK5
+// let elem = document.querySelector("h1");
+// elem.style.background = "green";
+//
+// let p1 = document.querySelector("p");
+// p1.style.fontWeight = "bold";
+//
+// let p2 = document.getElementById("p2");
+// p2.style.color = "red";
+//
+// let p3 = document.getElementById("p3");
+// p3.style.textDecoration = "underline";
+//
+// let p4 = document.getElementById("p4");
+// p4.style.fontStyle = "italic";
+//
+// let ul1 = document.getElementById("myList");
+// ul1.style.writingMode = "vertical-rl";
+//
+// let span1 = document.querySelector("span");
+// span1.style.wordSpacing = "-0.2em";
+
+//TASK6
+let message1 = prompt("Your message1 ...");
+let message2 = prompt("Your message2 ...");
+message1 = document.getElementById("input1").value = message1;
+message2 = document.getElementById("input2").value = message2;
+document.getElementById("input2").value = document.getElementById("input1").value;
+
+//TASK7
