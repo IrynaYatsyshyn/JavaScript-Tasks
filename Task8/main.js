79dcf19203e8aba1c7064beda97dcde3c51861c9
200221 <jacushin2002@gmail.com>
2021-04-22 17:24:41 +0300

    .

---
+++ Task8/main.js
@@ -0,0 +1,43 @@
+//TASK1
+// function checkReg(str) {
+//     let regExp = /[A-Z]/;
+//     if (regExp.test(str) == true) {
+//         alert("String's starts with uppercase character!");
+//
+//     } else {
+//         alert("String's not starts with uppercase character!");
+//     }
+// }
+//
+// checkReg("Hello");
+
+//TASK5
+// function checkCard(number) {
+//     let regExp =/(\-|\s)?(\d{4})(\-|\s)?(\d{4})(\-|\s)?(\d{4})(\-|\s)?(\d{4})/;
+//     let result = regExp.test(number);
+//     alert(result);
+// }
+//
+// checkCard("2345 5762 4086 4173");
+
+//TASK4
+// const str = 'Java Script';
+// alert(str.replace(/(\w+) (\w+)/i, '$2, $1'));
+
+
+
+/////////////////////////////////////////////////////////////not done yet
+//TASK3
+// let str = "cdbBdbsbz";
+
+//TASK2
+// function checkEmail(email) {
+//     let regExp = /(A-Z)([a-z0-9])(gmail.com)/;
+//     if (regExp.test(email) == true) {
+//         alert("TRUE");
+//     } else {
+//         alert("FALSE");
+//     }
+// }
+//
+// checkEmail("yatsyshyn05@gmail.com");
