69cdf6b7af840c41a4fb224adcdec86654633e74
200221 <jacushin2002@gmail.com>
2021-04-26 17:19:27 +0300

    .

--- Task8/main.js
+++ Task8/main.js
@@ -30,13 +30,39 @@
 // console.log(result);
 
 //TASK2
-function checkEmail(email) {
-    let regExp = /\S+@\S+.\S+/;
-    if (regExp.test(email) == true) {
-        alert("TRUE");
-    } else {
-        alert("FALSE");
-    }
-}
+// function checkEmail(email) {
+//     let regExp = /\S+@\S+.\S+/;
+//     if (regExp.test(email) == true) {
+//         alert("TRUE");
+//     } else {
+//         alert("FALSE");
+//     }
+// }
+//
+// checkEmail("Qmail2@gmail.com");
 
-checkEmail("Qmail2@gmail.com");
+//TASK6
+// function checkEmail(email) {
+//     let regExp = /^[A-Za-z0-9]+([-]?\w+)*@\w+([-]?\w+)+(\.\w{2,3})+$/;
+//     if (regExp.test(email)) {
+//         console.log("Correct email!");
+//     } else {
+//         console.log("Email is not correct!");
+//
+//     }
+// }
+//
+// checkEmail('#my_mail@gmail.com');
+// checkEmail('my_mail@gmail.com');
+
+//TASK7
+// function checkLogin(login) {
+//     const regExp = /^[A-Za-z][A-Za-z0-9\.]{1,9}$/;
+//     const regNum = /[0-9\.]+/g;
+//     const result = regExp.test(login);
+//     const resultNum = login.match(regNum);
+//     console.log(`${login} is ${result} - ${resultNum}`);
+// }
+//
+// checkLogin('ee1.1ret3');
+// checkLogin('ee1*1ret3');
