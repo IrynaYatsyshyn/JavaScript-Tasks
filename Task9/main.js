225b50825df7697971642dbb5101377a9b349948
200221 <jacushin2002@gmail.com>
2021-04-26 20:38:57 +0300

    finish

--- Task9/main.js
+++ Task9/main.js
@@ -18,48 +18,61 @@
 // console.log(name4); // "Bob"
 // console.log(age4); // 26
 
-//TASK3------------------------------------------
-// function mul(param) {
-//     if (param == Number) {
-//         console.log(mul(param));
-//     } else {
-//         console.log(param);
+//TASK3
+// function mul(...param) {
+//     let num;
+//     let result = 1;
+//     for (let i of param) {
+//         if (typeof i == "number") {
+//             result *= i;
+//             num = true;
+//         }
 //     }
+//     return num ? result : 0;
 // }
 //
-// mul(1, "str", 2, 3, true); // 6
-// mul(null, "str", false, true); // 0
+// console.log(mul(1, "str", 2, 3, true)); // 6
+// console.log(mul(null, "str", false, true)); // 0
 
 ///////////////////////////////////////////////
 //TASK4
-let server = {
-    data: 0,
-    convertToString: (callback) => {
-        callback(( () => {
-            return this.data + "";
-        })(this));
-    }
-};
-let client = {
-    server: server,
-    result: "",
-    calc:(data) => {
-        this.server.data = data;
-        this.server.convertToString(this.notification());
-    },
-    notification: () => {
-        return ((callback) =>{
-            this.result = callback();
-        })(this);
-    }
-};
-client.calc(123);
-console.log(client.result); // "123"
-console.log(typeof client.result); // "string"
+// let server = {
+//     data: 0,
+//     convertToString(callback) {
+//         callback(() => {
+//             return this.data + "";
+//         });
+//     }
+// };
+// let client = {
+//     server: server,
+//     result: "",
+//     calc(data) {
+//         this.server.data = data;
+//         this.server.convertToString(this.notification());
+//     },
+//     notification() {
+//         return ((callback) => {
+//             this.result = callback();
+//         });
+//     }
+// };
+// client.calc(123);
+// console.log(client.result); // "123"
+// console.log(typeof client.result); // "string"
 
 //TASK5
-//let keys = [1, 2, 3, 4];
+// let keys = [1, 2, 3, 4];
 // let values = ["div", "span", "b", "i"];
+//
+// function mapBuilder(keys, values) {
+//     let map = new Map();
+//     for(let i of keys) {
+//         map.set(i,values[i-1]);
+//     }
+//     return map;
+// }
+//
 // let map = mapBuilder(keys, values);
-// console.log(map.size); // 4
-// 	console.log(map.get(2)); // "span"
+// console.log(keys.length); // 4
+// console.log(map.get(2)); // "span"
